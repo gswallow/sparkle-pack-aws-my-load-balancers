e4384e1280e03f77077dbeec6854c2f0364135d3
Greg Swallow <gswallow@gmail.com>
2016-12-03 17:10:39 -0500

    compact! created a nil array.

--- lib/sparkleformation/components/elb_security_policies.rb
+++ lib/sparkleformation/components/elb_security_policies.rb
@@ -1,11 +1,10 @@
 require 'aws-sdk-core'
 
 client = ::Aws::ElasticLoadBalancing::Client.new()
-policy_descriptions = ::Array.new
-policy_descriptions = client.describe_load_balancer_policies.policy_descriptions
-policy_descriptions.collect do |d|
+policy_descriptions = ::Array.new(client.describe_load_balancer_policies.policy_descriptions)
+policy_descriptions = policy_descriptions.collect do |d|
   d['policy_name'] if d['policy_type_name'] == 'SSLNegotiationPolicyType'
-end.compact!.sort!
+end.compact.sort
 
 SparkleFormation.component(:elb_security_policies) do
   parameters(:elb_security_policy) do
