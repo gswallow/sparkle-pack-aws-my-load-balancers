5f501762cee3fd612ebdc83940649fceb379c63f
Greg Swallow <gswallow@gmail.com>
2016-12-03 16:44:30 -0500

    Auto-generate SSL security policies.

---
+++ lib/sparkleformation/components/elb_security_policies.rb
@@ -0,0 +1,15 @@
+require 'aws-sdk-core'
+
+client = ::Aws::ElasticLoadBalancing::Client.new()
+policy_descriptions = ::Array.new(client.describe_load_balancer_policies.policy_descriptions)
+policy_descriptions.collect do |d|
+  d['policy_name'] if d['policy_type_name'] == 'SSLNegotiationPolicyType'
+end.compact!.sort!
+
+SparkleFormation.component(:elb_security_policies) do
+  parameters(:elb_security_policy) do
+    type 'String'
+    allowed_values policy_descriptions
+    default policy_descriptions.last
+  end
+end
